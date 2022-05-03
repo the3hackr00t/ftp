478059b37e61e8b39eb339eb7a6d88a4b5997ad9
the3hackr00t <104789190+the3hackr00t@users.noreply.github.com>
2022-05-03 20:52:39 +0200

    Add files via upload

---
+++ pico.py
@@ -0,0 +1,36 @@
+import socket
+
+print("""       .__              
+______ |__| ____  ____  
+\____ \|  |/ ___\/  _ \ 
+|  |_> >  \  \__(  <_> )
+|   __/|__|\___  >____/ 
+|__|           \/       """)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+target = input('Enter a IP address: ') 
+  
+t_IP = socket.gethostbyname(target) 
+print("Scanning ", t_IP) 
+  
+  
+def port_scan(port): 
+    try: 
+        s.connect((t_IP, port)) 
+        return True
+    except: 
+        return False
+  
+
+def scanner():
+    port = int(input("Enter a port number: ")) 
+
+    if port_scan(port): 
+       print('Port', port, 'is open') 
+       scanner()
+    else: 
+       print("port", port, "is closed") 
+       scanner()
+
+scanner()
